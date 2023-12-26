e492e1a14893ea01bd19224db98f4644df620a5c
43DaNang <ledaothaotien92@gmail.com>
2023-12-26 20:10:20 +0700

    add cor

--- Backend/API/Program.cs
+++ Backend/API/Program.cs
@@ -2,16 +2,25 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<VHIDbContext>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("VHIConnectionString")));
-
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                      policy =>
+                      {
+                          policy.WithOrigins("https://localhost:8081"
+                                              );
+                      });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -26,5 +35,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseCors(MyAllowSpecificOrigins);
 app.Run();
