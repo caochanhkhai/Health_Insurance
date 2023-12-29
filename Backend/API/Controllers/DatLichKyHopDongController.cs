e1f689aaf30ae1ad11c7c0678ab706bdfce5801d
Cao Chanh Khai <caochanhkhai1111@gmail.com>
2023-12-29 15:43:26 +0700

    Backup before merge from main

---
+++ Backend/API/Controllers/DatLichKyHopDongController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using API.Data;
+using API.DTOs;
+using API.Domain;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DatLichKyHopDongController : ControllerBase
+    {
+        private readonly VHIDbContext VHIDbContext;
+
+        public DatLichKyHopDongController(VHIDbContext VHIDbContext)
+        {
+            this.VHIDbContext = VHIDbContext;
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetById(int id)
+        {
+            var datLichTuVan = VHIDbContext.DatLichTuVan.FirstOrDefault(a => a.ID_YeuCauTuVan == id);
+            if (datLichTuVan == null)
+            {
+                return NotFound();
+            }
+
+            var DatLichTuVanDto = new DatLichTuVanDTO
+            {
+                ID_YeuCauTuVan = datLichTuVan.ID_YeuCauTuVan,
+                TinhTrangDuyet = datLichTuVan.TinhTrangDuyet,
+                DiaDiem = datLichTuVan.DiaDiem,
+                ThoiGian = datLichTuVan.ThoiGian,
+//                KhachHangID_KhachHang = datLichTuVan.KhachHangID_KhachHang,
+//                NhanVien1ID_NhanVien = datLichTuVan.NhanVien1ID_NhanVien,
+//                NhanVien2ID_NhanVien = datLichTuVan.NhanVien2ID_NhanVien,
+            };
+
+            return Ok(DatLichTuVanDto);
+        }
+
+        [HttpPost]
+        public IActionResult CreateAppointment([FromBody] DatLichTuVanDTO dto)
+        {
+            var dLichTuVan = new DatLichTuVan
+            {
+                TinhTrangDuyet = dto.TinhTrangDuyet,
+                DiaDiem = dto.DiaDiem,
+                ThoiGian = dto.ThoiGian,
+//                KhachHangID_KhachHang = dto.KhachHangID_KhachHang,
+//                NhanVien1ID_NhanVien = dto.NhanVien1ID_NhanVien,
+//                NhanVien2ID_NhanVien = dto.NhanVien2ID_NhanVien,
+            };
+
+            VHIDbContext.DatLichTuVan.Add(dLichTuVan);
+            VHIDbContext.SaveChanges();
+
+            var createdDatLichTuVan = new DatLichTuVanDTO
+            {
+                ID_YeuCauTuVan = dLichTuVan.ID_YeuCauTuVan,
+                TinhTrangDuyet = dLichTuVan.TinhTrangDuyet,
+                DiaDiem = dLichTuVan.DiaDiem,
+                ThoiGian = dLichTuVan.ThoiGian,
+//                KhachHangID_KhachHang = dLichTuVan.KhachHangID_KhachHang,
+//                NhanVien1ID_NhanVien = dLichTuVan.NhanVien1ID_NhanVien,
+//                NhanVien2ID_NhanVien = dLichTuVan.NhanVien2ID_NhanVien,
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = createdDatLichTuVan.ID_YeuCauTuVan }, createdDatLichTuVan);
+        }
+    }
+}
---
+++ Backend/API/DTOs/DatLichTuVanDTO.cs
@@ -0,0 +1,16 @@
+﻿using API.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class DatLichTuVanDTO
+    {
+        public int ID_YeuCauTuVan { get; set; }
+        public string TinhTrangDuyet { get; set; }
+        public string DiaDiem { get; set; }
+        public DateTime ThoiGian { get; set; }
+        public int KhachHangID_KhachHang { get; set; }
+        public int NhanVien1ID_NhanVien { get; set; }
+        public int NhanVien2ID_NhanVien { get; set; }
+    }
+}
