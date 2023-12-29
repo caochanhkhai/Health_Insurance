91b657bc73d8c1d697994365a4cde62aac7713fc
nhatTuyet <chaunhattuyet022019@gmail.com>
2023-12-29 23:06:22 +0700

    addMigrationFile

---
+++ Backend/API/Migrations/20231228055519_AddNewColumnToDB.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNewColumnToDB : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NhanVienID_NhanVien",
+                table: "PhieuDangKi",
+                type: "int",
+                nullable: true,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "XacThuc",
+                table: "KhachHang",
+                type: "nvarchar(13)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "HinhAnh",
+                table: "GoiBaoHiem",
+                type: "varchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PhieuDangKi_NhanVienID_NhanVien",
+                table: "PhieuDangKi",
+                column: "NhanVienID_NhanVien");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PhieuDangKi_NhanVien_NhanVienID_NhanVien",
+                table: "PhieuDangKi",
+                column: "NhanVienID_NhanVien",
+                principalTable: "NhanVien",
+                principalColumn: "ID_NhanVien",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PhieuDangKi_NhanVien_NhanVienID_NhanVien",
+                table: "PhieuDangKi");
+
+            migrationBuilder.DropIndex(
+                name: "IX_PhieuDangKi_NhanVienID_NhanVien",
+                table: "PhieuDangKi");
+
+            migrationBuilder.DropColumn(
+                name: "NhanVienID_NhanVien",
+                table: "PhieuDangKi");
+
+            migrationBuilder.DropColumn(
+                name: "XacThuc",
+                table: "KhachHang");
+
+            migrationBuilder.DropColumn(
+                name: "HinhAnh",
+                table: "GoiBaoHiem");
+        }
+    }
+}
