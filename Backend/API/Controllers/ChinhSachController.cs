2e5e51117c956923ce81077b3025266c3ca103b0
nhatTuyet <chaunhattuyet022019@gmail.com>
2023-12-31 11:16:09 +0700

    Them GetAll, GetById cho GoiBaoHiem, ChinhSach, Update HinhAnh cho GoiBaoHiem

---
+++ Backend/API/Controllers/ChinhSachController.cs
@@ -0,0 +1,59 @@
+﻿using API.Data;
+using API.Domain;
+using API.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChinhSachController : ControllerBase
+    {
+        private readonly VHIDbContext VHIDbContext;
+
+        public ChinhSachController(VHIDbContext VHIDbContext)
+        {
+            this.VHIDbContext = VHIDbContext;
+        }
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IActionResult GetAll()
+        {
+            var dscsDomain = VHIDbContext.ChinhSach.ToList();
+            List<ChinhSachDTO> dscsDTO = new List<ChinhSachDTO>();
+            foreach (var cs in dscsDomain)
+            {
+                ChinhSachDTO cs_dto = CreateChinhSachDTO(cs);
+                dscsDTO.Add(cs_dto);
+            }
+
+            return Ok(dscsDTO);
+        }
+
+        [HttpGet]
+        [Route("id")]
+        public IActionResult GetById(int id)
+        {
+            var cs = VHIDbContext.ChinhSach.FirstOrDefault(x => x.ID_ChinhSach == id);
+            if (cs == null)
+            {
+                return NotFound("Không tìm thấy chính sách.");
+            }
+            ChinhSachDTO cs_dto = CreateChinhSachDTO(cs);
+
+            return Ok(cs_dto);
+        }
+
+        private static ChinhSachDTO CreateChinhSachDTO(ChinhSach? cs)
+        {
+            return new ChinhSachDTO()
+            {
+                ID_ChinhSach = cs.ID_ChinhSach,
+                STT = cs.STT,
+                TenChinhSach = cs.TenChinhSach,
+                ThoiGianPhatHanh = cs.ThoiGianPhatHanh
+            };
+        }
+    }
+}
--- Backend/API/Controllers/GoiBaoHiemController.cs
+++ Backend/API/Controllers/GoiBaoHiemController.cs
@@ -1,4 +1,6 @@
 ﻿using API.Data;
+using API.Domain;
+using API.DTOs;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
@@ -18,9 +20,62 @@ namespace API.Controllers
         [Route("GetAll")]
         public IActionResult GetAll()
         {
-            var goiBH = VHIDbContext.GoiBaoHiem.ToList();
-            return Ok(goiBH);
+            var dsgbhDomain = VHIDbContext.GoiBaoHiem.ToList();
+            List<GoiBaoHiemDTO> dsgbhDTO = new List<GoiBaoHiemDTO>();
+            foreach (var gbh in dsgbhDomain)
+            {
+                GoiBaoHiemDTO gbh_dto = CreateGoiBaoHiemDTO(gbh);
+                dsgbhDTO.Add(gbh_dto);
+            }
+
+            return Ok(dsgbhDTO);
         }
 
+        [HttpGet]
+        [Route("id")]
+        public IActionResult GetById(int id)
+        {
+            var gbh = VHIDbContext.GoiBaoHiem.FirstOrDefault(x => x.ID_GoiBaoHiem == id);
+            if (gbh == null)
+            {
+                return NotFound();
+            }
+            GoiBaoHiemDTO gbh_dto = CreateGoiBaoHiemDTO(gbh);
+
+            return Ok(gbh_dto);
+        }
+
+        [HttpPut]
+        [Route("UpdateHinhAnh(idgbh)")]
+        public IActionResult UpdateGoiBaoHiem_HinhAnh(int idgbh, string LinkHinhAnh)
+        {
+            var gbhDomain = VHIDbContext.GoiBaoHiem.FirstOrDefault(x => x.ID_GoiBaoHiem == idgbh);
+            if (gbhDomain == null)
+            {
+                return NotFound("Không tồn tại gói bảo hiểm này!");
+            }
+
+            gbhDomain.HinhAnh = LinkHinhAnh;
+
+            VHIDbContext.SaveChanges();
+            GoiBaoHiemDTO gbh_dto = CreateGoiBaoHiemDTO(gbhDomain);
+            return Ok(gbh_dto);
+        }
+
+        private static GoiBaoHiemDTO CreateGoiBaoHiemDTO(GoiBaoHiem? gbh)
+        {
+            return new GoiBaoHiemDTO()
+            {
+                ID_GoiBaoHiem = gbh.ID_GoiBaoHiem,
+                TenBaoHiem = gbh.TenBaoHiem,
+                TenGoi = gbh.TenGoi,
+                GiaTien = gbh.GiaTien,
+                ThoiHan = gbh.ThoiHan,
+                MoTa = gbh.MoTa,
+                NgayPhatHanh = gbh.NgayPhatHanh,
+                TinhTrang = gbh.TinhTrang,
+                HinhAnh = gbh.HinhAnh
+            };
+        }
     }
 }
