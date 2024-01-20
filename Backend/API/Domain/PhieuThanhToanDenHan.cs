6d4de5ecab50920bc2f1749cc4bb80ef28358ad3
nhatTuyet <chaunhattuyet022019@gmail.com>
2024-01-20 19:11:51 +0700

    Tao API Get PhieuThanhToanDenHan

---
+++ Backend/API/Domain/PhieuThanhToanDenHan.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Domain
+{
+    public class PhieuThanhToanDenHan
+    {
+        public int ID_HopDong { get; set; }
+        public string TenBaoHiem { get; set; }
+        public string TenGoi { get; set; }
+        public int SoNgayDenHan { get; set; }
+        public decimal SoTienCanDong { get; set; }
+    }
+}
