38d51f2ce4c4f683827b9d0422ceed62e7c6bec1
nhatTuyet <chaunhattuyet022019@gmail.com>
2024-01-20 19:42:35 +0700

    Tao API gui Email cho Khach Hang

---
+++ Backend/API/Controllers/SendEmailController.cs
@@ -0,0 +1,43 @@
+﻿using API.Data;
+using API.Helper;
+using API.MiddleWare;
+using API.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SendEmailController : ControllerBase
+    {
+        private readonly IEmailService emailService;
+        private readonly VHIDbContext VHIDbContext;
+        public SendEmailController(IEmailService service, VHIDbContext VHIDbContext)
+        {
+            this.emailService = service;
+            this.VHIDbContext = VHIDbContext;
+        }
+        [HttpPost("SendMail")]
+        public async Task<IActionResult> SendMail(int idKH, string chuDe, string noiDung)
+        {
+            var kh = VHIDbContext.KhachHang.FirstOrDefault(x=>x.ID_KhachHang == idKH);
+            string email = kh.Email;
+            try
+            {
+                var jwtService = new JwtService("vhihealthinsurance");
+                Mailrequest mailrequest = new Mailrequest()
+                {
+                    ToEmail = email,
+                    Subject = chuDe,
+                    Body = $"<div>{noiDung}</div>"
+                };
+                await emailService.SendEmailAsync(mailrequest);
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+            return Ok();
+        }
+    }
+}
